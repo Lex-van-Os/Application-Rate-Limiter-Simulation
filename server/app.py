a06c108921ee7bd622de9efbec7a4969f321501e
Lex van Os <vanoslex7@gmail.com>
2024-03-23 07:50:34 +0100

    Implementation of rate limiter

--- server/app.py
+++ server/app.py
@@ -1,28 +1,62 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from rate_limiter import SlidingWindowCounter
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address
+)
+
+window_size = 60  # 1 minute
+limit = 100  # 100 requests per minute
+
 CORS(app)
 
+limiter_counter = SlidingWindowCounter(window_size, limit)
 
+# Define rate limit rule using the sliding window counter
+
+
+@limiter.request_filter
+def sliding_window_rate_limit():
+    limiter_counter.add_request()
+    return limiter_counter.is_allowed()
+
+
+@sliding_window_rate_limit
 @app.route('/')
 def hello():
     return jsonify(message='Hello, this is your API!')
 
 
+@sliding_window_rate_limit
 @app.route('/GetOrderInformation')
 def getOrder():
-    return jsonify(message='GetOrderInformation')
+    if limiter_counter.is_allowed():
+        return jsonify(message='GetOrderInformation')
+    else:
+        return jsonify(error='Rate limit exceeded'), 429
 
 
+@sliding_window_rate_limit
 @app.route('/RequestOrderInvoices')
 def requestOrderInvoices():
-    return jsonify(message='RequestOrderInvoices')
+    if limiter_counter.is_allowed():
+        return jsonify(message='RequestOrderInvoices')
+    else:
+        return jsonify(error='Rate limit exceeded'), 429
 
 
+@sliding_window_rate_limit
 @app.route('/CreateOrder', methods=['POST'])
 def createOrder():
-    return jsonify(message='CreateOrder')
+    if limiter_counter.is_allowed():
+        return jsonify(message='CreateOrder')
+    else:
+        return jsonify(error='Rate limit exceeded'), 429
 
 
 if __name__ == '__main__':
